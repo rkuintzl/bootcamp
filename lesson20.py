0f3c8c18fba1ce6ead5d61dd1797be7c0c0b3fea
Rachael Kuintzle <rkuintzl@caltech.edu>
2016-09-13 11:29:21 -0700

    Added lesson 20 exercise.

---
+++ lesson20.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+xa_high = np.loadtxt('data/xa_high_food.csv'
+xa_low = np.loadtxt('data/xa_low_food.csv')
+
+def xa_to_diameter(xa):
+    """
+    Convert an array of cross-sectional areas
+    to diameters with commensurate units
+    """
+
+    # Compute diameter from area
+    # A = π * d**2 / 4
+    diameter = np.sqrt(xa * 4 / np.pi)
+    
