09146f3343e0af76f4bbec4c5aaf1e05429edb43
Rachael Kuintzle <rkuintzl@caltech.edu>
2016-09-13 16:10:34 -0700

    Completed lesson24.py

--- lesson24.py
+++ lesson24.py
@@ -27,18 +27,24 @@ xa_low = np.loadtxt('data/xa_low_food.csv')
 x_high, y_high = ecdf(xa_high)
 x_low, y_low = ecdf(xa_low)
 
+x = np.linspace(1600, 2500, 400) # xrange min, xrange max, number of points
+cdf_high = scipy.stats.norm.cdf(x, loc=np.mean(xa_high), scale = np.std(xa_high))
+cdf_low = scipy.stats.norm.cdf(x, loc=np.mean(xa_low), scale = np.std(xa_low))
+
 # Plot data
 plt.plot(x_low, y_low, marker='.', markersize=20, alpha=0.5)
 plt.plot(x_high, y_high, marker='.', markersize=20, alpha=0.5)
+# Plot smoothed points
+plt.plot(x, cdf_low, color='gray')
+plt.plot(x, cdf_high, color='gray')
+
 plt.legend(('low food', 'high food'), loc='lower right')
 # plt.errorbar(iptg, gfp, yerr=sem, marker='.',
 #             markersize=20)
 plt.xlabel('Cross-sectional area (µm)')
 plt.ylabel('eCDF')
 
-plt.savefig('lesson24_egg_area_eCDF.svg', bbox_inches='tight')
+#plt.savefig('lesson24_egg_area_eCDF.svg', bbox_inches='tight')
 
 plt.show()
 # plt.margins(0.05)
-# plt.xlim(x_min-1, x_max+1)
-# plt.show()
